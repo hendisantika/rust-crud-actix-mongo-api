ddf30db5b0d82b7ce7cd1aedc6f8d8ff0c3d3923
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 08:58:13 +0700

    Add User Struct in users models.rs 🫸🌀✏️📗🐧🐳

---
+++ src/users/models.rs
@@ -0,0 +1,20 @@
+use std::fmt;
+
+use actix_web::{FromRequest, HttpMessage, HttpRequest};
+use actix_web::dev::Payload;
+use bson::oid::ObjectId;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct User {
+    #[serde(rename = "_id")]
+    pub id: Option<ObjectId>,
+    pub email: String,
+    pub username: String,
+    pub password: String,
+    pub roles: Vec<Role>,
+    pub tokens: Option<Tokens>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
