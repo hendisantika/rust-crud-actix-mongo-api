3f134247af798b1c2294019e6fd15e1014d1db7b
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:10:02 +0700

    Add AuthResponse Struct 🫸🌀✏️📗🐧🐳

--- src/auth/models.rs
+++ src/auth/models.rs
@@ -7,3 +7,11 @@ pub struct AuthRequest {
     pub email: String,
     pub password: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct AuthResponse {
+    pub email: String,
+    pub username: String,
+    pub roles: Vec<Role>,
+    pub tokens: Tokens,
+}
