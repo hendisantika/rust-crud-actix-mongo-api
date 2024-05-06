6439256d2d2c75b6f99f4f1120c0f39fed3877fa
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 08:55:43 +0700

    Add new in Environment Impl 🫸🌀✏️📗🐧🐳

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -1,3 +1,5 @@
+use log::info;
+use mongodb::options::{ClientOptions, Credential};
 use mongodb::sync::Client;
 
 #[derive(Clone)]
@@ -13,4 +15,34 @@ pub struct Config {
     // ... other properties to be used throughout the app
 }
 
-impl Environment {}
+impl Environment {
+    pub async fn new() -> Self {
+        info!("[environment] Initializing environment");
+        dotenv::dotenv().ok();
+        log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found.");
+
+        let db_url = std::env::var("DB_URL").unwrap();
+        let db_name = std::env::var("DB_NAME").unwrap();
+        let db_username = std::env::var("DB_USER").unwrap();
+        let db_password = std::env::var("DB_PASSWORD").unwrap();
+        let host = std::env::var("HOST").unwrap();
+
+        let config = Config {
+            db_name: db_name.clone(),
+            host,
+        };
+
+        info!("[environment] Connecting to MongoDB at db_url={:?}, db_name={:?}", &db_url, &db_name);
+
+        let mut db_config = ClientOptions::parse(db_url).unwrap();
+        db_config.app_name = Some(String::from(&db_name));
+        db_config.server_selection_timeout = Some(std::time::Duration::new(5, 0));
+        db_config.credential = Some(Credential::builder().username(db_username).password(db_password).build());
+        let db_pool = Client::with_options(db_config).unwrap();
+
+        Self {
+            db_pool,
+            config,
+        }
+    }
+}
