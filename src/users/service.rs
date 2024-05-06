7335128665028e9860d83a889b7b6a0666ff7eab
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:02:40 +0700

    Add create in UserService Impl 🫸🌀✏️📗🐧🐳

--- src/users/service.rs
+++ src/users/service.rs
@@ -14,4 +14,30 @@ impl UserService {
         let collection: Collection<Document> = env.db().collection("users");
         UserService { collection }
     }
+
+    pub fn create(&self, request: CreateUser) -> Result<InsertOneResult, GenericError> {
+        let filter = bson::doc! {"username": &request.username };
+        // let filter = bson::doc! {"$or": [{"email": &request.email }, {"username": &request.username }]};
+        match self.collection.find_one(filter, None).unwrap() {
+            Some(_) => return Err(GenericError { message: "User already exists" }),
+            None => (),
+        }
+
+        let user = User {
+            id: None,
+            email: request.email,
+            username: request.username,
+            password: get_hashed_password(&request.password),
+            roles: request.roles,
+            tokens: None,
+            created_at: Some(Utc::now()),
+            updated_at: None,
+        };
+
+        let mut doc: Document = to_document(&user).unwrap();
+        doc.remove("_id"); // Remove None field that would be saved instead of auto-generated
+
+        let result: Result<InsertOneResult, Error> = self.collection.insert_one(doc, None);
+        Ok(result.unwrap())
+    }
 }
