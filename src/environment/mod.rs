783be6921d4d7c7cb9e5a9b9887b9e0a71a81524
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 08:54:58 +0700

    Add Config Struct 🫸🌀✏️📗🐧🐳

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -5,3 +5,10 @@ pub struct Environment {
     db_pool: Client,
     config: Config,
 }
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    db_name: String,
+    pub host: String,
+    // ... other properties to be used throughout the app
+}
