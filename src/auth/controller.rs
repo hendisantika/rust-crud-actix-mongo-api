5922fa89b0bac6cfdf8cf234662fd2cbaaada20e
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:12:38 +0700

    Add refresh in controller.rs 🫸🌀✏️📗🐧🐳

--- src/auth/controller.rs
+++ src/auth/controller.rs
@@ -12,3 +12,13 @@ pub async fn login(app_data: web::Data<crate::AppState>, body: web::Json<AuthReq
         Err(_) => HttpResponse::BadRequest().finish()
     }
 }
+
+#[post("/api/auth/refresh")]
+pub async fn refresh(app_data: web::Data<crate::AppState>, body: web::Json<TokenRefreshRequest>) -> impl Responder {
+    let result = web::block(move || app_data.auth_service.refresh(&body.into_inner().refresh_token)).await.unwrap();
+    match result {
+        Ok(data) => HttpResponse::Ok().json(data),
+        // Err(e) => HttpResponse::BadRequest().json(e.into())
+        Err(_) => HttpResponse::BadRequest().finish()
+    }
+}
