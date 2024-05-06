414fd93ca2dd485603d984842ebd66ee579c11ab
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:25:36 +0700

    Adjust the codes 🫸🌀✏️📗🐧🐳

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use actix_web::{App, HttpServer};
+use actix_web::web::Data;
+use actix_web_grants::GrantsMiddleware;
+
 use crate::auth::service::AuthService;
 use crate::environment::Environment;
 use crate::users::service::UserService;
@@ -9,6 +13,7 @@ mod users;
 mod security;
 
 mod test_controller;
+mod security;
 
 pub struct AppState {
     auth_service: AuthService,
@@ -22,4 +27,31 @@ impl AppState {
             user_service: UserService::new(env.clone()),
         }
     }
+}
+
+#[actix_rt::main]
+async fn main() -> Result<()> {
+    let env = Environment::new().await;
+    let host = &env.config().host.clone();
+
+    HttpServer::new(move || {
+        let app_state = AppState::new(env.clone());
+        App::new()
+            .app_data(Data::new(app_state))
+            .wrap(GrantsMiddleware::with_extractor(security::jwt_middleware))
+
+            .service(auth::controller::login)
+            .service(auth::controller::refresh)
+            .service(auth::controller::validate)
+
+            .service(users::controller::create)
+            .service(users::controller::get)
+
+            .service(test_controller::public)
+            .service(test_controller::protected_user)
+            .service(test_controller::protected_admin)
+    })
+        .bind(host)?
+        .run()
+        .await
 }
