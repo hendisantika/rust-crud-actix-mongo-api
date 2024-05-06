8d967229cc40262513d1e836e0e05560a7fc4281
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:04:50 +0700

    Add get in controller.rs 🫸🌀✏️📗🐧🐳

--- src/users/controller.rs
+++ src/users/controller.rs
@@ -11,3 +11,14 @@ pub async fn create(app_data: web::Data<crate::AppState>, body: web::Json<Create
         Err(_) => HttpResponse::BadRequest().finish()
     }
 }
+
+#[get("/api/user/{username}")]
+pub async fn get(app_data: web::Data<crate::AppState>, path: web::Path<String>) -> impl Responder {
+    let username = path.into_inner();
+    let result = web::block(move || app_data.user_service.get_by_username(&username)).await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        // Err(e) => HttpResponse::BadRequest().json::<GenericError>(e.into())
+        Err(_) => HttpResponse::BadRequest().finish()
+    }
+}
