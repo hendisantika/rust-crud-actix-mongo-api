3632b03045c3df20adcb7cfc53562a38799405f1
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:09:49 +0700

    Add AuthRequest Struct 🫸🌀✏️📗🐧🐳

--- src/auth/models.rs
+++ src/auth/models.rs
@@ -1,1 +1,9 @@
-s k
+use serde::{Deserialize, Serialize};
+
+use crate::users::models::{Role, Tokens};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct AuthRequest {
+    pub email: String,
+    pub password: String,
+}
