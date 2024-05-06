0df2ab0846665ad57537cbe070b00e983772ef43
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:12:27 +0700

    Add login in controller.rs 🫸🌀✏️📗🐧🐳

---
+++ src/auth/controller.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, HttpResponse, post, Responder, web};
+
+use crate::auth::models::{AuthRequest, TokenRefreshRequest};
+use crate::users::models::User;
+
+#[post("/api/auth/login")]
+pub async fn login(app_data: web::Data<crate::AppState>, body: web::Json<AuthRequest>) -> impl Responder {
+    let result = web::block(move || app_data.auth_service.login(body.into_inner())).await.unwrap();
+    match result {
+        Ok(data) => HttpResponse::Ok().json(data),
+        // Err(e) => HttpResponse::BadRequest().json(e.into())
+        Err(_) => HttpResponse::BadRequest().finish()
+    }
+}
