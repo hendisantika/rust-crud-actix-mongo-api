be72c6d55c03c979e8d39b0f7871cd2abb6e3e2d
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:01:51 +0700

    Add UserService Struct in service.rs 🫸🌀✏️📗🐧🐳

--- src/users/service.rs
+++ src/users/service.rs
@@ -1,3 +1,10 @@
+use bson::Document;
+use mongodb::sync::Collection;
+
 use crate::errors::GenericError;
 use crate::security::get_hashed_password;
 
+#[derive(Clone)]
+pub struct UserService {
+    collection: Collection<Document>,
+}
